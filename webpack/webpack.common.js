e1fa7715cb9bbd52c49934c80706b49d0b632457
Semih Raif Gürel <semihraifgurel@icloud.com>
2022-08-08 20:34:46 +0300

    prettier

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,50 +3,52 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const pages = ['index', 'example', 'a', 'b'];
+
 module.exports = {
-  entry: {
-    app: Path.resolve(__dirname, '../src/scripts/index.js'),
-  },
-  output: {
-    path: Path.join(__dirname, '../build'),
-    filename: 'js/[name].js',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      name: false,
-    },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
-    }),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
-      inject: "body"
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~': Path.resolve(__dirname, '../src'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        type: 'asset'
-      },
-    ],
-  },
+	entry: {
+		app: Path.resolve(__dirname, '../src/scripts/index.js'),
+	},
+	output: {
+		path: Path.join(__dirname, '../build'),
+		filename: 'js/[name].js',
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			name: false,
+		},
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
+		}),
+		new HtmlWebpackPlugin({
+			template: Path.resolve(__dirname, '../src/index.html'),
+			inject: 'body',
+		}),
+	],
+	resolve: {
+		alias: {
+			'~': Path.resolve(__dirname, '../src'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.mjs$/,
+				include: /node_modules/,
+				type: 'javascript/auto',
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+				type: 'asset',
+			},
+		],
+	},
 };
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -7,52 +7,52 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  target: 'web',
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
-  output: {
-    chunkFilename: 'js/[name].chunk.js',
-  },
-  devServer: {
-    client: {
-      logging: 'error',
-    },
-    hot: true,
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    new ESLintPlugin({
-      extensions: 'js',
-      emitWarning: true,
-      files: Path.resolve(__dirname, '../src'),
-    }),
-    new StylelintPlugin({
-      files: Path.join('src', '**/*.s?(a|c)ss'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
+	target: 'web',
+	mode: 'development',
+	devtool: 'eval-cheap-source-map',
+	output: {
+		chunkFilename: 'js/[name].chunk.js',
+	},
+	devServer: {
+		client: {
+			logging: 'error',
+		},
+		hot: true,
+	},
+	plugins: [
+		new Webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development'),
+		}),
+		new ESLintPlugin({
+			extensions: 'js',
+			emitWarning: true,
+			files: Path.resolve(__dirname, '../src'),
+		}),
+		new StylelintPlugin({
+			files: Path.join('src', '**/*.s?(a|c)ss'),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: Path.resolve(__dirname, '../src'),
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.s?css$/i,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
 });
